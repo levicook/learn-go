5add71e1e29e416662a4d3b6c45cce8286df5247
Levi Cook <levicook@gmail.com>
2009-11-15 22:40:27 -0600

    more failing test than you can shake a stick at

--- textile/textile_test.go
+++ textile/textile_test.go
@@ -1,5 +1,9 @@
 package textile
 
+import "io"
+import "json"
+import "os"
+import "strings"
 import "testing"
 
 var simplePhraseModifiers = map[string]string{
@@ -13,17 +17,47 @@ var simplePhraseModifiers = map[string]string{
 func TestBasicPhraseModifiers(t *testing.T) {
 	for input, expected := range simplePhraseModifiers {
 		observed, ok, errtok := TextileToHtml(input);
-		check(t, ok, errtok, expected, observed);
+		check(t, ok, errtok, input, expected, observed, "simplePhraseModifiers");
 	}
 }
 
+func TestRedClothFixtures(t *testing.T) {
+	dirName := "textile_test";
+	dirEntries, err := io.ReadDir(dirName);
+	doTheAngryErrorThing(t, err);
+	for _, de := range dirEntries {
+		if strings.HasSuffix(de.Name, "basic.json") {
+			bytes, err := io.ReadFile(dirName + "/" + de.Name);
+			doTheAngryErrorThing(t, err);
+			s := string(bytes);
+			json, ok, errtok := json.StringToJson(s);
+			if !ok {
+				t.Errorf("StringToJson(%#q) => error near %v", s, errtok)
+			}
+			for i := 0; i < json.Len(); i++ {
+				testCase := json.Elem(i);
+				name := testCase.Get("name").String() + " (" + de.Name + ")";
+				expected := testCase.Get("html").String();
+				input := testCase.Get("in").String();
+				observed, ok, errtok := TextileToHtml(input);
+				check(t, ok, errtok, input, expected, observed, name);
+			}
+		}
+	}
+}
+
+func doTheAngryErrorThing(t *testing.T, err os.Error) {
+	if err != nil {
+		t.Errorf(err.String())
+	}
+}
 
 //  internal utilities....
-func check(t *testing.T, ok bool, errtok string, expected string, observed string) {
+func check(t *testing.T, ok bool, errtok string, input string, expected string, observed string, name string) {
 	if !ok {
 		t.Fatalf("TextileToHtml failed near %s", errtok)
 	}
 	if expected != observed {
-		t.Errorf("Expected: %s\nObserved: %s", expected, observed)
+		t.Errorf("\nTest: %s\nInput: %s\nExpected: %s\nObserved: %s", name, input, expected, observed)
 	}
 }
